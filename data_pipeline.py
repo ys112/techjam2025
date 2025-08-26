e3b2f7e7af2805cbd7248c4c4e6ea25d984a1217
ys112 <yusian89@gmail.com>
2025-08-26 21:55:02 +0800

    Move docs into folder

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,15 +1,16 @@
-#%% Import Libraries
+# %% Import Libraries
 import gzip, json
 import pandas as pd
 from tabulate import tabulate
 
+
 def parse(path):
     with gzip.open(path, "rt", encoding="utf-8") as g:
         for line in g:
-         yield json.loads(line)
+            yield json.loads(line)
 
 
-#%% Main
+# %% Main
 # to print
 # for i, obj in enumerate(parse("review_South_Dakota.json.gz")):
 #     print(obj)
@@ -17,8 +18,10 @@ def parse(path):
 #         break
 
 #! 1) Load
-reviews_data = pd.read_json('review_South_Dakota.json.gz', lines=True, compression='gzip')         # or .json/.parquet
-biz_meta = pd.read_json('meta_South_Dakota.json.gz', lines=True, compression='gzip')
+reviews_data = pd.read_json(
+    "review_South_Dakota.json.gz", lines=True, compression="gzip"
+)  # or .json/.parquet
+biz_meta = pd.read_json("meta_South_Dakota.json.gz", lines=True, compression="gzip")
 
 # standardize columns
 biz_meta.columns = biz_meta.columns.str.lower().str.strip()
@@ -28,12 +31,11 @@ print("\n" + tabulate(reviews_data.head(10), headers="keys", tablefmt="psql"))
 print("\n" + tabulate(biz_meta.head(10), headers="keys", tablefmt="psql"))
 
 # 1. cleaning of review data
-#these columns are NOT NULL
+# these columns are NOT NULL
 reviews_data = reviews_data.dropna(subset=["text", "rating", "time", "gmap_id", "resp"])
 
 # pics/resp to booleans
 reviews_data["has_pics"] = reviews_data["pics"].notna()
-
 
 
 # 2. cleaning of meta data
@@ -45,18 +47,16 @@ biz_meta["price_level"] = biz_meta["price"].str.len()
 biz_meta["price_level"] = biz_meta["price_level"].fillna(0).astype("int8")
 
 
-
-
-
 # merge relevant cols from meta to reviews
 keep_cols = [
-    "gmap_id",        # join key
-    "name",           # business name
-    "category",       # type of business
-    "avg_rating",     # business-level avg
-    "num_of_reviews", # business popularity
-    "latitude", "longitude", # optional
-    "state"           # active/closed
+    "gmap_id",  # join key
+    "name",  # business name
+    "category",  # type of business
+    "avg_rating",  # business-level avg
+    "num_of_reviews",  # business popularity
+    "latitude",
+    "longitude",  # optional
+    "state",  # active/closed
 ]
 
 keep_cols = [c for c in keep_cols if c in biz_meta.columns]
